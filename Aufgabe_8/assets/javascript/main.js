75c22a11d456c8a6d90753a78994a4414cec4f64
YvonneJL <yvonneloder@gmx.de>
2024-12-09 14:56:14 +0100

    fertig

---
+++ Aufgabe_8/assets/javascript/main.js
@@ -0,0 +1,16 @@
+let x = "Hello";
+const y = "World";
+
+const z = x + y;
+document.write(`<p>${z}</p>`);
+
+const n = x + " " + y;
+document.write(n);
+
+x += " World";
+document.write(`<p>${x}</p>`);
+
+let meinString = "Ich bin Erste:r";
+meinString += " Ich komme auf Platz zwei";
+console.log(meinString);
+document.write(`<p>${meinString}</p>`);
